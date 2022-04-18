171a964811c86abb486ad3c71ccc5c0ea219793e
hui1010 <huiyi.skarner@gmail.com>
2022-04-18 20:57:41 +0200

    Finished Board part

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import Board from './components/Board';
+import Keyboard from './components/Keyboard';
+import { createContext, useState } from "react"
+import { boardDefault } from './Words';
+
+export const AppContext = createContext()
 
 function App() {
+  const [board, setBoard] = useState(boardDefault)
   return (
     <div className="App">
-      Hi huiyi
+      <nav>
+        <h1>Wordle</h1>
+      </nav>
+      <AppContext.Provider value={{ board, setBoard }}>
+        <Board />
+        <Keyboard />
+      </AppContext.Provider>
     </div>
   );
 }
---
+++ src/components/Letter.js
@@ -0,0 +1,12 @@
+import React, { useContext } from 'react'
+import { AppContext } from '../App'
+
+function Letter({ letterPos, attempVal }) {
+    const { board } = AppContext
+    const letter = board[attempVal][letterPos]
+    return (
+        <div className='letter'>{letter}</div>
+    )
+}
+
+export default Letter
