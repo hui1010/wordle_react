130d084a11d38b7a3ad7b7a4dbd7073cd5972bb1
hui1010 <huiyi.skarner@gmail.com>
2022-04-23 14:36:54 +0200

    Virtual keybaord function finished

--- src/components/Key.js
+++ src/components/Key.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AppContext } from "../App"
 
 function Key({ keyVal, bigKey }) {
+    const { onSelectLetter, onDelete, onEnter } = useContext(AppContext)
+
+    const selectLetter = () => {
+        if (keyVal === "Enter") {
+            onEnter()
+        } else if (keyVal === "Delete") {
+            onDelete()
+        } else {
+            onSelectLetter(keyVal)
+        }
+    }
+
     return (
-        <div className='key' id={bigKey && "big"}>{keyVal}</div>
+        <div className='key' id={bigKey && 'big'} onClick={selectLetter}>{keyVal}</div>
     )
 }
 
