d8b004b1f122bbc581be09277883dbe1897582ef
hui1010 <huiyi.skarner@gmail.com>
2022-04-23 13:52:10 +0200

    Keyboard layout done

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Key from "./Key"
 
 function Keyboard() {
     const keys1 = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
@@ -8,16 +9,20 @@ function Keyboard() {
     return (
         <div className='keyboard'>
             <div className='line1'>{
-                keys1.map(key => (<div>{key}</div>))
+                keys1.map(key => <Key keyVal={key} />)
             }</div>
             <div className='line2'>
                 {
-                    keys2.map(key => (<div>{key}</div>))
+                    keys2.map(key => <Key keyVal={key} />)
                 }
             </div>
-            <div className='line3'>{
-                keys3.map(key => (<div>{key}</div>))
-            }</div>
+            <div className='line3'>
+                <Key keyVal="Enter" bigKey />
+                {
+                    keys3.map(key => <Key keyVal={key} />)
+                }
+                <Key keyVal="Delete" bigKey />
+            </div>
 
         </div>
     )
