da590bc536ec96648da63a1aa8a59aa4e5a5139c
hui1010 <huiyi.skarner@gmail.com>
2022-04-23 15:18:32 +0200

    Actualy keyboard function done

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,25 +1,54 @@
-import React from 'react'
+import React, { useCallback, useEffect, useContext } from 'react'
 import Key from "./Key"
+import { AppContext } from '../App';
 
 function Keyboard() {
     const keys1 = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
     const keys2 = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
     const keys3 = ["Z", "X", "C", "V", "B", "N", "M"];
 
+    const { onSelectLetter, onDelete, onEnter } = useContext(AppContext)
+
+    const handleKeyboard = useCallback((e) => {
+        if (e.key === "Enter") {
+            onEnter()
+        } else if (e.key === "Backspace") {
+            onDelete()
+        } else {
+            keys1.forEach(key => {
+                if (e.key.toUpperCase() === key.toUpperCase()) onSelectLetter(key)
+            })
+            keys2.forEach(key => {
+                if (e.key.toUpperCase() === key.toUpperCase()) onSelectLetter(key)
+            })
+            keys3.forEach(key => {
+                if (e.key.toUpperCase() === key.toUpperCase()) onSelectLetter(key)
+            })
+        }
+    })
+
+    // How to detect keyboard event in React
+    useEffect(() => {
+        document.addEventListener("keydown", handleKeyboard)
+        return () => {
+            document.removeEventListener("keydown", handleKeyboard)
+        }
+    }, [handleKeyboard])
+
     return (
-        <div className='keyboard'>
+        <div className='keyboard' onKeyDown={handleKeyboard}>
             <div className='line1'>{
-                keys1.map(key => <Key keyVal={key} />)
+                keys1.map(key => <Key key={key} keyVal={key} />)
             }</div>
             <div className='line2'>
                 {
-                    keys2.map(key => <Key keyVal={key} />)
+                    keys2.map(key => <Key key={key} keyVal={key} />)
                 }
             </div>
             <div className='line3'>
                 <Key keyVal="Enter" bigKey />
                 {
-                    keys3.map(key => <Key keyVal={key} />)
+                    keys3.map(key => <Key key={key} keyVal={key} />)
                 }
                 <Key keyVal="Delete" bigKey />
             </div>
