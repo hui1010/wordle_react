270808b87262243ff3d430ceaaf83b42fdade2b8
hui1010 <huiyi.skarner@gmail.com>
2022-04-23 15:41:49 +0200

    Letter color fixed

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,44 +5,44 @@ function Board() {
     return (
         <div className='board'>
             <div className='row'>
-                <Letter letterPos={0} attempVal={0} />
-                <Letter letterPos={1} attempVal={0} />
-                <Letter letterPos={2} attempVal={0} />
-                <Letter letterPos={3} attempVal={0} />
-                <Letter letterPos={4} attempVal={0} />
+                <Letter letterPos={0} attemptVal={0} />
+                <Letter letterPos={1} attemptVal={0} />
+                <Letter letterPos={2} attemptVal={0} />
+                <Letter letterPos={3} attemptVal={0} />
+                <Letter letterPos={4} attemptVal={0} />
             </div>
             <div className='row'>
-                <Letter letterPos={0} attempVal={1} />
-                <Letter letterPos={1} attempVal={1} />
-                <Letter letterPos={2} attempVal={1} />
-                <Letter letterPos={3} attempVal={1} />
-                <Letter letterPos={4} attempVal={1} />
+                <Letter letterPos={0} attemptVal={1} />
+                <Letter letterPos={1} attemptVal={1} />
+                <Letter letterPos={2} attemptVal={1} />
+                <Letter letterPos={3} attemptVal={1} />
+                <Letter letterPos={4} attemptVal={1} />
             </div>
             <div className='row'>
-                <Letter letterPos={0} attempVal={2} />
-                <Letter letterPos={1} attempVal={2} />
-                <Letter letterPos={2} attempVal={2} />
-                <Letter letterPos={3} attempVal={2} />
-                <Letter letterPos={4} attempVal={2} />
+                <Letter letterPos={0} attemptVal={2} />
+                <Letter letterPos={1} attemptVal={2} />
+                <Letter letterPos={2} attemptVal={2} />
+                <Letter letterPos={3} attemptVal={2} />
+                <Letter letterPos={4} attemptVal={2} />
             </div>
             <div className='row'>
-                <Letter letterPos={0} attempVal={3} />
-                <Letter letterPos={1} attempVal={3} />
-                <Letter letterPos={2} attempVal={3} />
-                <Letter letterPos={3} attempVal={3} />
-                <Letter letterPos={4} attempVal={3} /></div>
+                <Letter letterPos={0} attemptVal={3} />
+                <Letter letterPos={1} attemptVal={3} />
+                <Letter letterPos={2} attemptVal={3} />
+                <Letter letterPos={3} attemptVal={3} />
+                <Letter letterPos={4} attemptVal={3} /></div>
             <div className='row'>
-                <Letter letterPos={0} attempVal={4} />
-                <Letter letterPos={1} attempVal={4} />
-                <Letter letterPos={2} attempVal={4} />
-                <Letter letterPos={3} attempVal={4} />
-                <Letter letterPos={4} attempVal={4} /></div>
+                <Letter letterPos={0} attemptVal={4} />
+                <Letter letterPos={1} attemptVal={4} />
+                <Letter letterPos={2} attemptVal={4} />
+                <Letter letterPos={3} attemptVal={4} />
+                <Letter letterPos={4} attemptVal={4} /></div>
             <div className='row'>
-                <Letter letterPos={0} attempVal={5} />
-                <Letter letterPos={1} attempVal={5} />
-                <Letter letterPos={2} attempVal={5} />
-                <Letter letterPos={3} attempVal={5} />
-                <Letter letterPos={4} attempVal={5} />
+                <Letter letterPos={0} attemptVal={5} />
+                <Letter letterPos={1} attemptVal={5} />
+                <Letter letterPos={2} attemptVal={5} />
+                <Letter letterPos={3} attemptVal={5} />
+                <Letter letterPos={4} attemptVal={5} />
             </div>
         </div>
     )
--- src/components/Letter.js
+++ src/components/Letter.js
@@ -1,11 +1,16 @@
 import React, { useContext } from 'react'
 import { AppContext } from '../App'
 
-function Letter({ letterPos, attempVal }) {
-    const { board } = useContext(AppContext)
-    const letter = board[attempVal][letterPos]
+function Letter({ letterPos, attemptVal }) {
+
+    const { board, correctWord, currAttempt } = useContext(AppContext)
+    const letter = board[attemptVal][letterPos]
+
+    const correct = correctWord[letterPos] === letter
+    const almost = !correct && letter !== "" && correctWord.includes(letter)
+    const letterState = currAttempt.attempt > attemptVal && (correct ? "correct" : almost ? "almost" : "error")
     return (
-        <div className='letter'>{letter}</div>
+        <div className='letter' id={letterState}>{letter}</div>
     )
 }
 
